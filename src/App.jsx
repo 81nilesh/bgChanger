4ec8cd62a4c031c9962a0743ca488871a5bb3663
Nilesh choudhary <121803888+81nilesh@users.noreply.github.com>
2024-05-28 13:26:36 +0530

    Project done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 function App() {
-  const [color, setColor] = useState("olive")
+  const [color, setColor] = useState("olive");
   return (
     <div className="w-full h-screen duration-200" style={{ backgroundColor: color }}>
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
